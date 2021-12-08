74d39eb3500d22454403b8934b4d056a945c39ef
Igor <swebdesigne@gmail.com>
2021-12-08 10:33:27 +0300

    remove static at the init method of the Main class

--- wp-content/themes/promtech-career/engine/Main.php
+++ wp-content/themes/promtech-career/engine/Main.php
@@ -1,7 +1,7 @@
 <?
 
 class Main {
-    public static function init($className, $initMethod, $param = '')
+    public function init($className, $initMethod, $param = '')
     {
         if (class_exists($className)) return new $className($initMethod, $param);
         else throw new \Exception("Класс $className не найден");
